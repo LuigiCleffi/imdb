a8e28b0bad2f72789d15e38c98b995404ea2cc57
LuigiCleffi <luigicleffi674@gmail.com>
2023-07-30 15:20:52 -0300

    Add loading effect

---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+export default function Loading() {
+  return (
+    <div className='flex justify-center'>
+      <img className='h-28' src="spinner.svg" alt="loading" />
+    </div>
+  )
+}
